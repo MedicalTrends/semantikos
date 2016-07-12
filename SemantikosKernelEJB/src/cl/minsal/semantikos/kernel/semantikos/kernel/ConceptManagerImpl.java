ca04150bb412cac8d843d8b8d0f70e67991385bd
Andres Farias <andres.farias@blueprintsit.cl>
2016-07-12 15:13:34 -0400

    Interfaces e implementación desde el prototipo.

---
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/semantikos/kernel/ConceptManagerImpl.java
@@ -0,0 +1,115 @@
+package cl.minsal.semantikos.kernel.semantikos.kernel;
+
+import cl.minsal.semantikos.model.Description;
+
+import javax.ejb.Stateless;
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by stk-des01 on 01-06-16.
+ */
+@Stateless
+public class ConceptManagerImpl implements ConceptManagerInterface {
+
+    String driver = "org.postgresql.Driver";
+    String ruta = "jdbc:postgresql://192.168.0.221:5432/postgres";
+    String user = "postgres";
+    String password = "1q2w3e";
+
+    @Override
+    public ArrayList<Description> findDescriptionForPattern(String pattern) {
+
+        ArrayList<Description> descriptions = new ArrayList<Description>();
+
+
+/*
+        try {
+            Class.forName(driver);
+            Connection conne = (Connection) DriverManager.getConnection(ruta, user, password);
+            CallableStatement call = conne.prepareCall("{call find_description(?,?)}");
+
+            call.setString(1, "");
+            call.setString(2, pattern);
+            call.execute();
+
+            ResultSet rs = call.getResultSet();
+
+            int idD;
+            String description;
+
+            while (rs.next()) {
+                idD = Integer.parseInt(rs.getString("id_concepto"));
+                description = rs.getString("term");
+                descriptions.add(new Description(idD, description));
+            }
+            conne.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+*/
+
+        return descriptions;
+    }
+
+    @Override
+    public String addConcept(String idCategory, boolean isValid) {
+        String idConcepto=null;
+
+
+
+/*
+        try {
+            Class.forName(driver);
+            Connection conne = (Connection) DriverManager.getConnection(ruta, user, password);
+            CallableStatement call = conne.prepareCall("{call crea_concepto(?,?)}");
+
+            call.setInt(1, Integer.parseInt(idCategory));
+            call.setBoolean(2, isValid);
+
+            call.execute();
+
+
+            ResultSet rs = call.getResultSet();
+            while (rs.next()) {
+                idConcepto = rs.getString(1);
+
+            }
+            conne.close();
+        } catch (SQLException | ClassNotFoundException e) {
+
+            System.out.println(e.toString());
+        }
+*/
+
+        return idConcepto;
+    }
+
+    @Override
+    public int getIDConceptBy(int idDescription) {
+
+        int idDes=0;
+
+/*
+        try {
+            Class.forName(driver);
+            Connection conne = (Connection) DriverManager.getConnection(ruta, user, password);
+            CallableStatement call = conne.prepareCall("{call get_concept_by_description_id(?)}");
+
+            call.setInt(1, idDescription);
+            call.execute();
+
+            ResultSet rs = call.getResultSet();
+            while (rs.next()) {
+                idDes = Integer.parseInt(rs.getString(1));
+            }
+            conne.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+*/
+
+        return idDes;
+    }
+}
