13ff79f81153b45e0c2327a6c3da9f148dee0cb5
Francisco Mendez <francisco.mendez@blueprintsit.cl>
2016-07-12 16:23:57 -0400

    commit inicial

---
+++ SemantikosWeb/src/main/java/cl/minsal/semantikos/designmodelweb/test/PersistenceTestMBean.java
@@ -0,0 +1,16 @@
+package cl.minsal.semantikos.designmodelweb.test;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * Created by BluePrints Developer on 12-07-2016.
+ */
+
+@ManagedBean
+@ViewScoped
+public class PersistenceTestMBean {
+
+
+
+}
