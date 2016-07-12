12b88b76e6eb5232aea09a83d942b3d350c22455
Gustavo Punucura <gustavopunucura@gmail.com>
2016-07-12 10:48:29 -0400

    3er commit

--- SemantikosWeb/src/main/java/cl/minsal/semantikos/auth/AuthenticationFilter.java
+++ SemantikosWeb/src/main/java/cl/minsal/semantikos/auth/AuthenticationFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 /**
  * Created by Francisco Mendez on 19-05-2016.
