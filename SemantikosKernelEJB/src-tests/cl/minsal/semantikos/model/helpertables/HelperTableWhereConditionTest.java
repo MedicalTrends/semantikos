bc89f3c763625d4f49bc689764436eeab278e114
Andres Farias <andres.farias@blueprintsit.cl>
2016-09-06 15:43:23 -0300

    Funciones de HelperTable para poder recuperar registros desde las tablas.

---
+++ SemantikosKernelEJB/src-tests/cl/minsal/semantikos/model/helpertables/HelperTableWhereConditionTest.java
@@ -0,0 +1,23 @@
+package cl.minsal.semantikos.model.helpertables;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
+import java.sql.Timestamp;
+
+import static cl.minsal.semantikos.model.helpertables.ConditionalOperator.GREATER_OR_GREATER_THAN;
+import static cl.minsal.semantikos.model.helpertables.HelperTable.SYSTEM_COLUMN_VALIDITY_UNTIL;
+import static org.junit.Assert.assertTrue;
+
+public class HelperTableWhereConditionTest {
+
+    @Test
+    public void testToString() throws Exception {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        HelperTableWhereCondition<Timestamp> validityUntilCondition = new HelperTableWhereCondition<>(SYSTEM_COLUMN_VALIDITY_UNTIL, GREATER_OR_GREATER_THAN, now);
+
+        String representation = validityUntilCondition.toString();
+        assertTrue(representation.startsWith(SYSTEM_COLUMN_VALIDITY_UNTIL.getColumnName() + " > "));
+    }
+}
