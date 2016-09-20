60ab4602a4357ce5a8955e14cd08a026a52e9235
Gustavo Punucura <gustavopunucura@gmail.com>
2016-09-20 16:25:02 -0300

    Refset bean

--- SemantikosWeb/src/cl/minsal/semantikos/admin_refset/RefSetsBean.java
+++ SemantikosWeb/src/cl/minsal/semantikos/admin_refset/RefSetsBean.java
@@ -1,7 +1,34 @@
 package cl.minsal.semantikos.admin_refset;
 
+import cl.minsal.semantikos.kernel.components.RefSetManager;
+import cl.minsal.semantikos.model.RefSet;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.List;
+
 /**
  * Created by des01c7 on 20-09-16.
  */
+@ManagedBean(name="refsetsBean")
+@ViewScoped
 public class RefSetsBean {
+
+
+    private RefSet refSetToCreate;
+
+    private List<RefSet> refSetList;
+
+    @EJB
+    private RefSetManager refSetManager;
+
+
+    @PostConstruct
+    public void init(){
+
+
+    }
+
 }
