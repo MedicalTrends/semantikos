1d14582175419b3afad9c5b6a8e4c4ba46501b50
Francisco Mendez <francisco.mendez@blueprintsit.cl>
2016-09-21 18:59:39 -0300

    Merge branch 'master' of https://github.com/MedicalTrends/semantikos
    # Conflicts:
    #	SemantikosWeb/web/resources/smtk/helperTableType.xhtml
    merge raro, odio a git

---
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/model/browser/ConceptQueryFilter.java
@@ -0,0 +1,15 @@
+package cl.minsal.semantikos.model.browser;
+
+import cl.minsal.semantikos.model.relationships.Target;
+import cl.minsal.semantikos.model.relationships.TargetDefinition;
+
+import java.util.List;
+
+/**
+ * Created by BluePrints Developer on 21-09-2016.
+ */
+public class ConceptQueryFilter {
+
+    TargetDefinition definition;
+    List<Target> targets;
+}
