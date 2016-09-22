9643efb4647e71475d9842fef2ee0f880cc4c07f
Francisco Mendez <francisco.mendez@blueprintsit.cl>
2016-09-22 16:10:07 -0300

    merge de refsets a master
    # Conflicts:
    #	SemantikosWeb/web/views/concept/conceptEdit.xhtml

---
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/ConceptQueryDAOImpl.java
@@ -0,0 +1,16 @@
+package cl.minsal.semantikos.kernel.daos;
+
+import cl.minsal.semantikos.model.ConceptSMTK;
+import cl.minsal.semantikos.model.browser.ConceptQuery;
+
+import java.util.List;
+
+/**
+ * Created by BluePrints Developer on 22-09-2016.
+ */
+public class ConceptQueryDAOImpl implements ConceptQueryDAO {
+    @Override
+    public List<ConceptSMTK> callQuery(ConceptQuery query) {
+        return null;
+    }
+}
