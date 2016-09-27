d0e1960fbf2bba9d57d5ef8bb080d39e261e29f7
Francisco Mendez <francisco.mendez@blueprintsit.cl>
2016-09-27 16:29:47 -0300

    implementacion base query conceptos, 2

--- SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/ConceptQueryDAOImpl.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/ConceptQueryDAOImpl.java
@@ -38,17 +38,34 @@ public class ConceptQueryDAOImpl implements ConceptQueryDAO {
 
         //TODO: hacer funcion en pg
         try (Connection connection = connect.getConnection();
-             CallableStatement call = connection.prepareCall("{call semantikos.get_all_concepts(?,?,?)}")) {
+             CallableStatement call = connection.prepareCall("{call semantikos.find_concept_by_query(?,?,?,?,?,?,?,?,?,?,?) " )){
 
-            String sql = "select * from semantikos.smtk_concept c" +
-                    "where " +
-                    "c.mod ";
-            CallableStatement call2 = connection.prepareCall(sql);
+            /*
+            1 categories _int4,
+            2 pattern text,
+            3 page int4,
+            4 page_size int4,
+            5 modeled bool,
+            6 review bool,
+            7 consult bool,
+            8 tag_id int4,
+            9 creation_date_from date,
+            10 creation_date_to date,
+            11 orden text
+            */
 
 
-            call.setBoolean(1, query.getModeled());
-            call.setInt(2, query.getPageNumber());
-            call.setInt(3, query.getPageSize());
+            call.setArray(1, getArrayCategories(query, connection));
+            call.setString(2,query.getQuery() );
+            call.setInt(3, query.getPageNumber());
+            call.setInt(4, query.getPageSize());
+            call.setBoolean(5, query.getModeled());
+            call.setBoolean(6, query.getToBeReviewed());
+            call.setBoolean(7, query.getToBeConsulted());
+            call.setInt(8, query.getTagId());
+            call.setDate(9, new Date(query.getCreationDateSince().getTime()));
+            call.setDate(10, new Date(query.getCreationDateTo().getTime()));
+            call.setString(11, query.getOrder());
             call.execute();
 
             ResultSet rs = call.getResultSet();
@@ -81,6 +98,13 @@ public class ConceptQueryDAOImpl implements ConceptQueryDAO {
         return null;
     }
 
+    private Array getArrayCategories(ConceptQuery query, Connection connection) throws SQLException {
+        Long[] categorias = new Long[query.getCategories().size()];
+        for(int i = 0; i<= query.getCategories().size();i++){
+            categorias[i]=query.getCategories().get(i).getId();
+        }
+        return connection.createArrayOf("integer", categorias);
+    }
 
 
     private String makeSQLfromQuery(ConceptQuery query){
