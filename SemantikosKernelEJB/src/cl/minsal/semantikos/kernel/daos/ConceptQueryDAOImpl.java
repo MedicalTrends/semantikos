3f9f509a74b0bc5a96038b5604978f9898e82aa1
Francisco Mendez <francisco.mendez@blueprintsit.cl>
2016-09-28 18:14:15 -0300

    completa implementacion de querys con filtros de tablas auxiliares.

--- SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/ConceptQueryDAOImpl.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/ConceptQueryDAOImpl.java
@@ -4,6 +4,9 @@ import cl.minsal.semantikos.kernel.components.ConceptManager;
 import cl.minsal.semantikos.kernel.util.ConnectionBD;
 import cl.minsal.semantikos.model.ConceptSMTK;
 import cl.minsal.semantikos.model.browser.ConceptQuery;
+import cl.minsal.semantikos.model.browser.ConceptQueryFilter;
+import cl.minsal.semantikos.model.helpertables.HelperTable;
+import cl.minsal.semantikos.model.helpertables.HelperTableRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,8 +32,6 @@ public class ConceptQueryDAOImpl implements ConceptQueryDAO {
     public List<ConceptSMTK> callQuery(ConceptQuery query) {
 
 
-        String sqlquery = "";
-
         List<ConceptSMTK> concepts = new ArrayList<ConceptSMTK>();
 
         ConnectionBD connect = new ConnectionBD();
@@ -38,34 +39,40 @@ public class ConceptQueryDAOImpl implements ConceptQueryDAO {
 
         //TODO: hacer funcion en pg
         try (Connection connection = connect.getConnection();
-             CallableStatement call = connection.prepareCall("{call semantikos.find_concept_by_query(?,?,?,?,?,?,?,?,?,?,?) " )){
+             CallableStatement call = connection.prepareCall("{call semantikos.find_concept_by_query(?,?,?,?,?,?,?,?,?,?,?,?,?,?) " )){
 
             /*
-            1 categories _int4,
-            2 pattern text,
-            3 page int4,
-            4 page_size int4,
-            5 modeled bool,
-            6 review bool,
-            7 consult bool,
-            8 tag_id int4,
-            9 creation_date_from date,
-            10 creation_date_to date,
-            11 orden text
+               1 categories _int4,
+               2 pattern text,
+               3 auxiliary_targets [][],
+               4 auxiliary_tables _int4,
+               5 auxiliary_reldefs _int4,
+               6 modeled bool,
+               7 review bool,
+               8 consult bool,
+               9 tag_id int4,
+               10 creation_date_from date,
+               11 creation_date_to date,
+               12 orden text,
+               13 page int4,
+               14 page_size int4
             */
 
 
             call.setArray(1, getArrayCategories(query, connection));
             call.setString(2,query.getQuery() );
-            call.setInt(3, query.getPageNumber());
-            call.setInt(4, query.getPageSize());
-            call.setBoolean(5, query.getModeled());
-            call.setBoolean(6, query.getToBeReviewed());
-            call.setBoolean(7, query.getToBeConsulted());
-            call.setInt(8, query.getTagId());
-            call.setDate(9, new Date(query.getCreationDateSince().getTime()));
-            call.setDate(10, new Date(query.getCreationDateTo().getTime()));
-            call.setString(11, query.getOrder());
+            call.setArray(3, getArrayAuxTargets(query, connection));
+            call.setArray(4, getArrayAuxTables(query, connection));
+            call.setArray(5, getArrayAuxRefdefs(query, connection));
+            call.setBoolean(6, query.getModeled());
+            call.setBoolean(7, query.getToBeReviewed());
+            call.setBoolean(8, query.getToBeConsulted());
+            call.setLong(9, query.getTag().getId());
+            call.setDate(10, new Date(query.getCreationDateSince().getTime()));
+            call.setDate(11, new Date(query.getCreationDateTo().getTime()));
+            call.setString(12, query.getOrder());
+            call.setInt(13, query.getPageNumber());
+            call.setInt(14, query.getPageSize());
             call.execute();
 
             ResultSet rs = call.getResultSet();
@@ -97,6 +104,89 @@ public class ConceptQueryDAOImpl implements ConceptQueryDAO {
 
         return null;
     }
+
+    private Array getArrayAuxTargets(ConceptQuery query, Connection connection) throws SQLException {
+
+
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+
+
+        int maxTargetsSize = 0;
+        int filters = 0;
+
+
+        for (ConceptQueryFilter filter:query.getFilters()) {
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                filters++;
+
+                if (filter.getTargets().size()>maxTargetsSize)
+                    maxTargetsSize = filter.getTargets().size();
+            }
+        }
+
+
+        int[][] auxtargets = new int[filters][maxTargetsSize];
+
+        int i = 0;
+        for (ConceptQueryFilter filter:query.getFilters()) {
+
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                for(int j = 0; j < filter.getTargets().size(); j++){
+                    auxtargets[i][j] = (int) ( ( (HelperTableRecord) (filter.getTargets().get(j)) ).getId() );
+                }
+
+                i++;
+
+            }
+        }
+        return connection.createArrayOf("integer", auxtargets);
+    }
+
+
+    private Array getArrayAuxRefdefs(ConceptQuery query, Connection connection) throws SQLException {
+
+        int filters = 0;
+
+        for (ConceptQueryFilter filter:query.getFilters()) {
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                filters++;
+            }
+        }
+
+        Integer[] auxtables = new Integer[filters];
+
+        int i = 0;
+        for (ConceptQueryFilter filter:query.getFilters()) {
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                auxtables[i] =  (int)   filter.getDefinition().getId()   ;
+                i++;
+            }
+        }
+        return connection.createArrayOf("integer", auxtables);
+    }
+
+    private Array getArrayAuxTables(ConceptQuery query, Connection connection) throws SQLException {
+
+        int filters = 0;
+
+        for (ConceptQueryFilter filter:query.getFilters()) {
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                filters++;
+            }
+        }
+
+        Integer[] auxtables = new Integer[filters];
+
+        int i = 0;
+        for (ConceptQueryFilter filter:query.getFilters()) {
+            if (filter.getDefinition().getTargetDefinition().isHelperTable()) {
+                auxtables[i] =  (int) (  ((HelperTable)filter.getDefinition().getTargetDefinition()).getId()   );
+                i++;
+            }
+        }
+        return connection.createArrayOf("integer", auxtables);
+    }
+
 
     private Array getArrayCategories(ConceptQuery query, Connection connection) throws SQLException {
         Long[] categorias = new Long[query.getCategories().size()];
