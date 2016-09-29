f923ef15ba145f249b81922687a68edd5a0297e6
diego1982chile <diego.abelardo.soto@gmail.com>
2016-09-29 16:55:59 -0300

    Browser

--- SemantikosWeb/src/cl/minsal/semantikos/designer_modeler/browser/ConceptBrowserBean.java
+++ SemantikosWeb/src/cl/minsal/semantikos/designer_modeler/browser/ConceptBrowserBean.java
@@ -124,9 +124,9 @@ public class ConceptBrowserBean implements Serializable {
             @Override
             public List<ConceptSMTK> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
 
-                List<ConceptSMTK> conceptSMTKs = conceptManager.findConceptBy(category, first, pageSize);
+                //List<ConceptSMTK> conceptSMTKs = conceptManager.findConceptBy(category, first, pageSize);
 
-                //List<ConceptSMTK> conceptSMTKs = conceptQueryManager.executeQuery(conceptQuery);
+                List<ConceptSMTK> conceptSMTKs = conceptQueryManager.executeQuery(conceptQuery);
                 this.setRowCount(30);
 
                 return conceptSMTKs;
